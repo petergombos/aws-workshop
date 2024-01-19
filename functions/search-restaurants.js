bd3468ec1367fb05a0a2f7ae9799f11de8598a1e
Peter Gombos <peter@tabeo.co.uk>
2024-01-19 17:30:35 +0100

    Add SSM.

--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -2,8 +2,13 @@ const { DynamoDB } = require("@aws-sdk/client-dynamodb")
 const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb")
 const dynamodbClient = new DynamoDB()
 const dynamodb = DynamoDBDocumentClient.from(dynamodbClient)
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
 
-const defaultResults = parseInt(process.env.default_results)
+const middyCacheEnabled = JSON.parse(process.env.middy_cache_enabled)
+const middyCacheExpiry = parseInt(process.env.middy_cache_expiry_milliseconds)
+
+const { serviceName, stage } = process.env
 const tableName = process.env.restaurants_table
 
 const findRestaurantsByTheme = async (theme, count) => {
@@ -19,14 +24,21 @@ const findRestaurantsByTheme = async (theme, count) => {
   return resp.Items
 }
 
-module.exports.handler = async (event, context) => {
+module.exports.handler = middy(async (event, context) => {
   const req = JSON.parse(event.body)
   const theme = req.theme
-  const restaurants = await findRestaurantsByTheme(theme, defaultResults)
+  const restaurants = await findRestaurantsByTheme(theme, context.config.defaultResults)
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants)
   }
 
   return response
-}
+}).use(ssm({
+  cache: middyCacheEnabled,
+  cacheExpiry: middyCacheExpiry,
+  setToContext: true,
+  fetchData: {
+    config: `/${serviceName}/${stage}/search-restaurants/config`
+  }
+}))
