4a9b57340df650fa9bf0ea80ed326ac56b228a58
Peter Gombos <peter@tabeo.co.uk>
2024-01-22 16:43:19 +0100

    Add eventbridge integration to order submit and notify restaurant.

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,54 +1,64 @@
-const fs = require("fs")
-const Mustache = require('mustache')
-const http = require('axios')
-const aws4 = require('aws4')
-const URL = require('url')
+const fs = require("fs");
+const Mustache = require("mustache");
+const http = require("axios");
+const aws4 = require("aws4");
+const URL = require("url");
 
-const restaurantsApiRoot = process.env.restaurants_api
-const cognitoUserPoolId = process.env.cognito_user_pool_id
-const cognitoClientId = process.env.cognito_client_id
-const awsRegion = process.env.AWS_REGION
+const restaurantsApiRoot = process.env.restaurants_api;
+const ordersApiRoot = process.env.orders_api;
+const cognitoUserPoolId = process.env.cognito_user_pool_id;
+const cognitoClientId = process.env.cognito_client_id;
+const awsRegion = process.env.AWS_REGION;
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-const template = fs.readFileSync('static/index.html', 'utf-8')
+const template = fs.readFileSync("static/index.html", "utf-8");
 
 const getRestaurants = async () => {
-  console.log(`loading restaurants from ${restaurantsApiRoot}...`)
-  const url = URL.parse(restaurantsApiRoot)
+  console.log(`loading restaurants from ${restaurantsApiRoot}...`);
+  const url = URL.parse(restaurantsApiRoot);
   const opts = {
     host: url.hostname,
-    path: url.pathname
-  }
+    path: url.pathname,
+  };
 
-  aws4.sign(opts)
+  aws4.sign(opts);
 
   const httpReq = http.get(restaurantsApiRoot, {
-    headers: opts.headers
-  })
-  return (await httpReq).data
-}
+    headers: opts.headers,
+  });
+  return (await httpReq).data;
+};
 
 module.exports.handler = async (event, context) => {
-  const restaurants = await getRestaurants()
-  console.log(`found ${restaurants.length} restaurants`)  
-  const dayOfWeek = days[new Date().getDay()]
+  const restaurants = await getRestaurants();
+  console.log(`found ${restaurants.length} restaurants`);
+  const dayOfWeek = days[new Date().getDay()];
   const view = {
     awsRegion,
     cognitoUserPoolId,
     cognitoClientId,
     dayOfWeek,
     restaurants,
-    searchUrl: `${restaurantsApiRoot}/search`
-  }
-  const html = Mustache.render(template, view)
+    searchUrl: `${restaurantsApiRoot}/search`,
+    placeOrderUrl: ordersApiRoot,
+  };
+  const html = Mustache.render(template, view);
   const response = {
     statusCode: 200,
     headers: {
-      'content-type': 'text/html; charset=UTF-8'
+      "content-type": "text/html; charset=UTF-8",
     },
-    body: html
-  }
+    body: html,
+  };
 
-  return response
-}
+  return response;
+};
