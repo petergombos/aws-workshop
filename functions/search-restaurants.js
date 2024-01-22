9042daefeee5c883383f2025f2ffb31548c22a9a
Peter Gombos <peter@tabeo.co.uk>
2024-01-22 14:45:04 +0100

    Add secret via SSM.

--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -1,44 +1,58 @@
-const { DynamoDB } = require("@aws-sdk/client-dynamodb")
-const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb")
-const dynamodbClient = new DynamoDB()
-const dynamodb = DynamoDBDocumentClient.from(dynamodbClient)
-const middy = require('@middy/core')
-const ssm = require('@middy/ssm')
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  ScanCommand,
+} = require("@aws-sdk/lib-dynamodb");
+const dynamodbClient = new DynamoDB();
+const dynamodb = DynamoDBDocumentClient.from(dynamodbClient);
+const middy = require("@middy/core");
+const ssm = require("@middy/ssm");
 
-const middyCacheEnabled = JSON.parse(process.env.middy_cache_enabled)
-const middyCacheExpiry = parseInt(process.env.middy_cache_expiry_milliseconds)
+const middyCacheEnabled = JSON.parse(process.env.middy_cache_enabled);
+const middyCacheExpiry = parseInt(process.env.middy_cache_expiry_milliseconds);
 
-const { serviceName, ssmStage } = process.env
-const tableName = process.env.restaurants_table
+const { serviceName, ssmStage } = process.env;
+const tableName = process.env.restaurants_table;
 
 const findRestaurantsByTheme = async (theme, count) => {
-  console.log(`finding (up to ${count}) restaurants with the theme ${theme}...`)
+  console.log(
+    `finding (up to ${count}) restaurants with the theme ${theme}...`
+  );
 
-  const resp = await dynamodb.send(new ScanCommand({
-    TableName: tableName,
-    Limit: count,
-    FilterExpression: "contains(themes, :theme)",
-    ExpressionAttributeValues: { ":theme": theme }
-  }))
-  console.log(`found ${resp.Items.length} restaurants`)
-  return resp.Items
-}
+  const resp = await dynamodb.send(
+    new ScanCommand({
+      TableName: tableName,
+      Limit: count,
+      FilterExpression: "contains(themes, :theme)",
+      ExpressionAttributeValues: { ":theme": theme },
+    })
+  );
+  console.log(`found ${resp.Items.length} restaurants`);
+  return resp.Items;
+};
 
 module.exports.handler = middy(async (event, context) => {
-  const req = JSON.parse(event.body)
-  const theme = req.theme
-  const restaurants = await findRestaurantsByTheme(theme, context.config.defaultResults)
+  console.info(context.secretString);
+  const req = JSON.parse(event.body);
+  const theme = req.theme;
+  const restaurants = await findRestaurantsByTheme(
+    theme,
+    context.config.defaultResults
+  );
   const response = {
     statusCode: 200,
-    body: JSON.stringify(restaurants)
-  }
+    body: JSON.stringify(restaurants),
+  };
 
-  return response
-}).use(ssm({
-  cache: middyCacheEnabled,
-  cacheExpiry: middyCacheExpiry,
-  setToContext: true,
-  fetchData: {
-    config: `/${serviceName}/${ssmStage}/search-restaurants/config`
-  }
-}))
+  return response;
+}).use(
+  ssm({
+    cache: middyCacheEnabled,
+    cacheExpiry: middyCacheExpiry,
+    setToContext: true,
+    fetchData: {
+      config: `/${serviceName}/${ssmStage}/search-restaurants/config`,
+      secretString: `/${serviceName}/${ssmStage}/search-restaurants/secretString`,
+    },
+  })
+);
